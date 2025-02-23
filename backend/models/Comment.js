9ccd8a964f7671e82108e4c6758fa3e71c7e7ad1
ricoa21 <rikspresso@gmail.com>
2025-02-23 00:06:20 +0000

    commit

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -5,6 +5,23 @@ const Comment = sequelize.define("Comment", {
   content: {
     type: DataTypes.TEXT,
     allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: "Comment content cannot be empty",
+      },
+      len: {
+        args: [1, 1000],
+        msg: "Comment must be between 1 and 1000 characters long",
+      },
+    },
+  },
+  UserId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Users",
+      key: "id",
+    },
   },
 });
 
