89a87a21a08708fa6b23982b37e7ac73af37f5b7
ricoa21 <rikspresso@gmail.com>
2025-02-23 01:14:09 +0000

    commit password

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,12 @@
 // config/database.js
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("commentary_database", "postgres", null, {
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  "commentary_database",
+  "ricoa21",
+  "Stockholm%28",
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
 
 module.exports = sequelize;
