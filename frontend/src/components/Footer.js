098613de1e80ec862414abf73f498bcca4eff705
ricoa21 <rikspresso@gmail.com>
2025-02-26 14:36:16 +0000

    commit header footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+// src/components/Footer.js
+import React from 'react';
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2025 Live Commentary Platform</p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+// src/components/Header.js
+import React from 'react';
+const Header = () => {
+  return (
+    <header>
+      <h1>Live Commentary Platform</h1>
+    </header>
+  );
+};
+export default Header;
