48408be80db44476e7799b5cb586c700d10c99c0
ricoa21 <rikspresso@gmail.com>
2025-03-26 14:28:19 +0000

    UpcomingFixtures commit

--- frontend/src/components/UpcomingFixtures.js
+++ frontend/src/components/UpcomingFixtures.js
@@ -13,8 +13,8 @@ const UpcomingFixtures = () => {
           'http://localhost:4000/api/fixtures/between',
           {
             params: {
-              startDate: '2025-04-02',
-              endDate: '2025-04-03',
+              startDate: '2025-04-05',
+              endDate: '2025-04-06',
             },
           }
         );
